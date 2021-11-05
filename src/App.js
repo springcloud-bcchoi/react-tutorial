9fabfadb3a9d355bad3ec625595a9c21313f158b
springcloud-bcchoi <bcchoi@aspringcloud.com>
2021-11-05 22:42:01 +0900

    lean useEffect #2

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import {useState, useEffect} from "react";
 
 function App() {
     const [count, setCount] = useState(0);
+    const [keyWord, setKeyWord] = useState("");
     const onClick = () => setCount((c) => c + 1)
-    console.log("1")
+    console.log("i run all the time")
     const iRun = () => {
-        console.log("0");
+        console.log("CALL THE API...");
+    }
+    const onChange = (event) => {
+        setKeyWord(event.target.value);
     }
     useEffect(iRun, [])
+    useEffect(()=>{
+        if(keyWord !== "" && keyWord.length > 5) {
+            console.log("SEARCH FOR", keyWord);
+        }
+    }, [keyWord] )
     return (
         <div>
-
+            <input value={keyWord} onChange={onChange} type={"text"} placeholder={"Search for ..."}/>
             <h1 className={styles.title}>{count}</h1>
             <button onClick={onClick}>Click me!</button>
 
