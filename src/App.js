a92d873a28506d35c97df9ef8aa323c76f08aa24
springcloud-bcchoi <bcchoi@aspringcloud.com>
2021-11-05 21:25:58 +0900

    lean useEffect #1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Button from "./Button"
 import styles from "./App.module.css";
+import {useState, useEffect} from "react";
 
 
 function App() {
+    const [count, setCount] = useState(0);
+    const onClick = () => setCount((c) => c + 1)
+    console.log("1")
+    const iRun = () => {
+        console.log("0");
+    }
+    useEffect(iRun, [])
     return (
         <div>
 
-            <h1 className={styles.title}>Welcome back!</h1>
-            <Button text={"test"} />
+            <h1 className={styles.title}>{count}</h1>
+            <button onClick={onClick}>Click me!</button>
 
         </div>
     );
