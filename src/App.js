d9f29381ffe9251fe9f8982268d8f7b9093e86a9
springcloud-bcchoi <bcchoi@aspringcloud.com>
2021-11-05 23:10:02 +0900

    destroy using useEffect

--- src/App.js
+++ src/App.js
@@ -2,32 +2,27 @@ import Button from "./Button"
 import styles from "./App.module.css";
 import {useState, useEffect} from "react";
 
+function Hello() {
+    useEffect(()=>{
+        console.log("HELLO");
+        return () => { console.log("BYE BYE")}
+    }, [])
+    return (
+        <h1>Hello</h1>
+    )
+}
 
 function App() {
-    const [count, setCount] = useState(0);
-    const [keyWord, setKeyWord] = useState("");
-    const onClick = () => setCount((c) => c + 1)
-    console.log("i run all the time")
-    const iRun = () => {
-        console.log("CALL THE API...");
-    }
-    const onChange = (event) => {
-        setKeyWord(event.target.value);
-    }
-    useEffect(iRun, [])
-    useEffect(()=>{
-        if(keyWord !== "" && keyWord.length > 5) {
-            console.log("SEARCH FOR", keyWord);
-        }
-    }, [keyWord] )
+    const [showing, setShowing] = useState(false);
+    const onClick = () => setShowing((c) => !c)
     return (
         <div>
-            <input value={keyWord} onChange={onChange} type={"text"} placeholder={"Search for ..."}/>
-            <h1 className={styles.title}>{count}</h1>
-            <button onClick={onClick}>Click me!</button>
-
+            {
+                showing ? <Hello/> : null
+            }
+            <button onClick={onClick}>{showing ? "HIDE" : "SHOW"}</button>
         </div>
-    );
+    )
 }
 
 export default App;
